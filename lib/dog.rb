3d65a6ff042bb8675b3215e1b83101eb5efde763
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-06 05:55:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,7 +10,29 @@ class Dog
     @breed = breed
     @id = id
   end
-
+  
+  def save
+    dogs_name = self.name
+    dogs_breed = self.breed
+    dogs_id = self.id
+    
+    sql_insert = "INSERT INTO dogs(name, breed) VALUES('#{dogs_name}', '#{dogs_breed}'"
+    sql_update = "UPDATE dogs SET name = '#{dogs_name}' WHERE id = '#{dogs_id}'"
+    
+    if dogs_id != nil
+      DB[:conn].execute(sql_update)
+    elsif dogs_id == nil
+    DB[:conn].execute(sql_insert)
+      
+  end
+  
+  def self.drop_table
+    sql_drop =  <<-SQL
+        DROP TABLE IF EXISTS dogs
+    SQL
+    DB[:conn].execute(sql_drop)
+  end
+  
   def self.create_table
     sql_create =  <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
